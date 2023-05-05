351876734acd8c27165a9fa9b361890042967713
Thoraya-S <thoraya.s.j@gmail.com>
2023-05-05 19:11:13 +0400

    new changes

--- Backend/byanat-towers/src/main/java/byanattowersappws/model/Tower.java
+++ Backend/byanat-towers/src/main/java/byanattowersappws/model/Tower.java
@@ -2,16 +2,71 @@ package byanattowersappws.model;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
 public class Tower implements Serializable{
 private static final long serialVersionUID = 1L;
 	
-	private Integer towerId;
+	private Integer tower_id;
 	private String operator;
 	private String address; 
 	private Integer height;
-	private String towerType;
+	private String tower_type;
 	private Long latitude;
 	private Long longitude;
 	private String technology;
+	public Integer getTower_id() {
+		return tower_id;
+	}
+	public void setTower_id(Integer tower_id) {
+		this.tower_id = tower_id;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getHeight() {
+		return height;
+	}
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+	public String getTower_type() {
+		return tower_type;
+	}
+	public void setTower_type(String tower_type) {
+		this.tower_type = tower_type;
+	}
+	public Long getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Long latitude) {
+		this.latitude = latitude;
+	}
+	public Long getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Long longitude) {
+		this.longitude = longitude;
+	}
+	public String getTechnology() {
+		return technology;
+	}
+	public void setTechnology(String technology) {
+		this.technology = technology;
+	}
+	
 
 }
--- Backend/byanat-towers/src/main/java/byanattowersappws/service/TowerServiceImpl.java
+++ Backend/byanat-towers/src/main/java/byanattowersappws/service/TowerServiceImpl.java
@@ -3,6 +3,7 @@ package byanattowersappws.service;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,39 +12,45 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import byanattowersappws.model.Response;
 import byanattowersappws.model.Tower;
 
 @Service
 public class TowerServiceImpl implements TowerService{
 	
-
+	ObjectMapper mapper;
+	
 	@Override
 	public ResponseEntity<Tower> getAllTowers() {
 		
 		String url = "https://byanat.wiremockapi.cloud/api/v3/towers";
 		RestTemplate restTemplate = new RestTemplate();
-		
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-			
-		//Tower towersEntity = new Tower();
 		HttpEntity request = new HttpEntity(headers);
-			//HttpEntity<Tower> request = new HttpEntity<>(towersEntity, headers);
-		//	Tower tower = restTemplate.postForObject(url, request, Tower.class);
-		//	System.out.print(tower);
-			
 		//	ResponseEntity<Tower> response = restTemplate.postForEntity(url, request, Tower.class);
-		ResponseEntity<Tower[]> response = restTemplate.exchange(url, HttpMethod.GET,request,Tower[].class);
-		Tower[] towerlist = response.getBody();
-		System.out.print(towerlist[0]);
-			if (response.getStatusCode() == HttpStatus.OK) {
-			    System.out.println("Request Successful");
-			} else {
-				System.out.println("Request Failed");
-			}
+//		ResponseEntity<Tower[]> response = restTemplate.getForEntity(url,Tower[].class);
+//		Tower[] towerlist = response.getBody();
+		
+		/*ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,request, String.class);
+		
+		System.out.print(response.getBody().toString());*/
+		
+		ResponseEntity<List<Tower>> response1= restTemplate.exchange(url, 
+                HttpMethod.GET,request,
+        new ParameterizedTypeReference<List<Tower>>(){}); 
+		
+		List<Tower> employees = response1.getBody();
+		System.out.print(employees);
+		
+		//List<Tower> listTower = mapper.readValue(response.getBody(), Tower.class);
+//			if (response.getStatusCode() == HttpStatus.OK) {
+//			    System.out.println("Request Successful");
+//			} else {
+//				System.out.println("Request Failed");
+//			}
 		return null;
 	}
 
