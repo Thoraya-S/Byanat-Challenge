e393107ed04eb7ff910448197b4d4abc1d891523
Thoraya-S <thoraya.s.j@gmail.com>
2023-05-09 17:17:13 +0400

    Get tower by  id.

--- Backend/byanat-towers/src/main/java/byanattowersappws/controller/TowerController.java
+++ Backend/byanat-towers/src/main/java/byanattowersappws/controller/TowerController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import byanattowersappws.model.Tower;
 import byanattowersappws.service.TowerService;
+import jakarta.websocket.server.PathParam;
 
 
 @RestController
@@ -26,6 +28,14 @@ public class TowerController {
 	@PostMapping("searchTower")
 	public List<Tower> getSearchTower(@RequestBody Tower tower){
 		return towerService.getSearchTower(tower);
+	}
+	
+	@PostMapping("tower")
+	public List<Tower> getTowersByParameter(@RequestBody Tower tower, 
+			@PathParam("tower_id") Integer tower_id){
+		tower.setTower_id(tower_id);
+		return towerService.getSearchTower(tower);
+		
 	}
 	
 	
